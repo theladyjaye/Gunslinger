6a1b660b491a58567206f6de4f79a9d8cb0fed60
Phil Tobias <philtobias@gmail.com>
2010-09-13 16:09:45 -0700

    added sammy js

--- application_server/www/resources/js/gunslinger.main.js
+++ application_server/www/resources/js/gunslinger.main.js
@@ -1,13 +1,5 @@
 $(function() {
 	
-	// nav
-	$("#nav").delegate("a", "click", function() {
-		var page = $(this).attr("href").substr(1);
-		$(".page").hide();
-		$("#" + page).show();
-		return false;
-	});
-	
 	$("#dd-console").change(function() {
 		var val = $(this).val();
 		
@@ -16,4 +8,40 @@ $(function() {
 	});
 	
 	$("#datepicker").datepicker();
+	
+	// run sammy
+	app.run("#/lobby");
+	
+});
+
+function change_page(page_id)
+{
+	$(".page").filter(function() {
+		return $(this).attr("id") != page_id;
+	}).fadeOut();
+	
+	$("#" + page_id).delay(300).fadeIn();
+	
+	change_nav(page_id);
+}
+
+function change_nav(page_id)
+{
+	$("#nav").find("a").removeClass("on");
+	$("#nav a[href=#/" + page_id + "]").addClass("on");
+}
+
+// sammy
+var app = $.sammy(function() {
+	// turn off logging
+    Sammy.log = this.log = function() {};
+	
+	// routes
+	this.get("#/lobby", function() {
+		change_page("lobby");
+	}).get("#/schedule", function() {
+		change_page("schedule");
+	}).get("#/profile", function() {
+		change_page("profile");
+	});
 });
