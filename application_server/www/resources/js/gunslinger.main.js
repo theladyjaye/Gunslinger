7809b54d1c7c7e2ca20df27eda6df33d5ebc4947
Phil Tobias <philtobias@gmail.com>
2010-09-13 15:08:16 -0700

    schedule a game, lobby pages

--- application_server/www/resources/js/gunslinger.main.js
+++ application_server/www/resources/js/gunslinger.main.js
@@ -1,9 +1,19 @@
 $(function() {
+	
+	// nav
+	$("#nav").delegate("a", "click", function() {
+		var page = $(this).attr("href").substr(1);
+		$(".page").hide();
+		$("#" + page).show();
+		return false;
+	});
+	
 	$("#dd-console").change(function() {
 		var val = $(this).val();
 		
-		$(".game-table .console").parents("li").hide();
-		$(".game-table .console." + val).parents("li").show();
-		
+		$("#lobby .game-table .console").parents("li").hide();
+		$("#lobby .game-table .console." + val).parents("li").show();
 	});
+	
+	$("#datepicker").datepicker();
 });
