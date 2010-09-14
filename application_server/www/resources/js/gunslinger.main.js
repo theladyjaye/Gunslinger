baf84e378cbfb3b92f70302f97323ecc81db033a
Phil Tobias <philtobias@gmail.com>
2010-09-14 10:58:58 -0700

    fixed time in game view to be scheduled time, added elapsed time func, sample data works

--- application_server/www/resources/js/gunslinger.main.js
+++ application_server/www/resources/js/gunslinger.main.js
@@ -160,6 +160,7 @@ var utils = {
 		date = new Date(date);
 		
 		var hours = date.getHours() + 1,
+			minutes = date.getMinutes().toString(),
 			meridian = "am";
 		
  		if(hours > 12)
@@ -167,8 +168,70 @@ var utils = {
 			hours = hours % 12;
 			meridian = "pm";
 		}
+
+		if(minutes.length < 2)
+		{
+			minutes = "0" + minutes;
+		}
 		
-		return hours + ":" + date.getMinutes() + " " + meridian;
+		return hours + ":" + minutes + " " + meridian;
+	},
+	elapsed_time: function(created_at)
+	{
+		var now 	= new Date(),
+			created = new Date();
+			
+		created.setFullYear(created_at.substr(0, 4));
+		created.setMonth(created_at.substr(5, 2) - 1);
+		created.setDate(created_at.substr(8, 2));
+		created.setHours(created_at.substr(11, 2));
+		created.setMinutes(created_at.substr(14, 2));
+		created.setSeconds(created_at.substr(17, 2));			
+
+		now.setHours(now.getHours() - 1);
+		var age_in_seconds = (created.getTime() - now.getTime()) / 1000;
+
+		var s = function(n) { 
+			return n == 1 ? '' : 's' 
+		};
+		
+	    if (age_in_seconds < 0) 
+		{
+	        return 'just now';
+	    }
+	    if (age_in_seconds < 60) 
+		{
+	        var n = age_in_seconds;
+	        return n + ' second' + s(n) + ' ago';
+	    }
+	    if (age_in_seconds < 60 * 60) 
+		{
+	        var n = Math.floor(age_in_seconds/60);
+	        return n + ' minute' + s(n) + ' ago';
+	    }
+	    if (age_in_seconds < 60 * 60 * 24) 
+		{
+	        var n = Math.floor(age_in_seconds/60/60);
+	        return n + ' hour' + s(n) + ' ago';
+	    }
+	    if (age_in_seconds < 60 * 60 * 24 * 7)
+	 	{
+	        var n = Math.floor(age_in_seconds/60/60/24);
+	        return n + ' day' + s(n) + ' ago';
+	    }
+	    if (age_in_seconds < 60 * 60 * 24 * 31) 
+		{
+	        var n = Math.floor(age_in_seconds/60/60/24/7);
+	        return n + ' week' + s(n) + ' ago';
+	    }
+	    if (age_in_seconds < 60 * 60 * 24 * 365) 
+		{
+	        var n = Math.floor(age_in_seconds/60/60/24/31);
+	        return n + ' month' + s(n) + ' ago';
+	    }
+	    var n = Math.floor(age_in_seconds/60/60/24/365);
+	
+	    return n + ' year' + s(n) + ' ago';
 	},
 	get_open_spots: function(players)
 	{
